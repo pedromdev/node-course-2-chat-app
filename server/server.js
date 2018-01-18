62981406c6cfd582c3ccf83af7f4c7e45feedcd2
Pedro Alves <pedromarcelodesaalves@gmail.com>
2018-01-18 14:48:50 -0300

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -22,6 +22,12 @@ io.on('connection', (socket) => {
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
