9f996a77412e0564d57d8cb952fc0cc582499ebb
Pedro Alves <pedromarcelodesaalves@gmail.com>
2018-01-18 15:03:13 -0300

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -15,9 +15,15 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
   socket.emit('newMessage', {
-    from: 'Carol',
-    text: 'See you then',
-    createdAt: 123
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
   });
 
   socket.on('createMessage', (message) => {
@@ -28,6 +34,12 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
